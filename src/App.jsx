d869de1763f06f02cd0ea3a04435625d03b68acb
luhamoza <luqmanhafizimohdzawawi@gmail.com>
2023-06-26 07:43:07 +0800

    build: search input

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,46 @@ import "./app.css";
 
 const App = () => {
   const [recipe, setRecipe] = useState(resipi);
+  const [search, setSearch] = useState("");
+  const inputHandler = (e) => {
+    setSearch(e.target.value);
+  };
   return (
     <>
-      <h1>resepi dapoq pdot</h1>
+      <div className="container">
+        <h1>resepi dapoq pdot</h1>
+        <input
+          type="text"
+          placeholder="cari resepi.."
+          onChange={inputHandler}
+        />
+      </div>
       <section className="recipelist">
-        {recipe.map((recipe) => {
-          const { id, resipi, link, img } = recipe;
-          return (
-            <article key={id} className="recipe">
-              <h3>{resipi}</h3>
-              <a href={link}>
-                <img src={img} alt="gambar makanan" />
-              </a>
-            </article>
-          );
-        })}
+        {recipe
+          .filter((recipe) => {
+            if (search === "") {
+              return recipe;
+            } else if (
+              recipe.resipi
+                .toLocaleLowerCase()
+                .includes(search.toLocaleLowerCase())
+            ) {
+              return recipe;
+            }
+          })
+          .map((recipe) => {
+            const { id, resipi, link, img } = recipe;
+            return (
+              <article key={id} className="recipe">
+                <a href={link}>
+                  <h3>{resipi}</h3>
+                </a>
+                <a href={link}>
+                  <img src={img} alt="gambar makanan" />
+                </a>
+              </article>
+            );
+          })}
       </section>
     </>
   );
