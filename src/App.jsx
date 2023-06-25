682b5cf751841466a9eb6d0012fafea7eb4e27af
luhamoza <luqmanhafizimohdzawawi@gmail.com>
2023-06-26 05:02:51 +0800

    build:list of recipe and update styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,26 @@
+import { useState } from "react";
+import resipi from "./resipi";
+import "./app.css";
+
 const App = () => {
-  return <div>App</div>;
+  const [recipe, setRecipe] = useState(resipi);
+  return (
+    <>
+      <h1>resepi dapoq pdot</h1>
+      <section className="recipelist">
+        {recipe.map((recipe) => {
+          const { id, resipi, link, img } = recipe;
+          return (
+            <article key={id} className="recipe">
+              <h3>{resipi}</h3>
+              <a href={link}>
+                <img src={img} alt="gambar makanan" />
+              </a>
+            </article>
+          );
+        })}
+      </section>
+    </>
+  );
 };
 export default App;
